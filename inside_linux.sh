a010b438c18b4a4763c4b0c0546538ff3543509a
Frederico Flores <nagualcode@users.noreply.github.com>
2020-10-29 22:26:36 -0300

    Update inside_linux.sh

--- inside_linux.sh
+++ inside_linux.sh
@@ -3,3 +3,11 @@
 sudo apt update && sudo apt upgrade
 
 sudo apt install ffmpeg imagemagick
+
+curl https://rclone.org/install.sh | sudo bash
+
+sudo curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
+sudo chmod a+rx /usr/local/bin/youtube-dl
+sudo ln -s /usr/bin/python3 /usr/local/bin/python
+
+echo 'export PATH="~/nagualtools:$PATH"' >>~/.bashrc
